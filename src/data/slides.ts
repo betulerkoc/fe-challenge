8d4ab1fe3fe386eb3c2f98a9696fcc004ae7086a
Betul Erkoc <bet.er@hotmail.com>
2025-05-12 13:44:45 -0400

    add slide data

---
+++ src/data/slides.ts
@@ -0,0 +1,32 @@
+export const slides = [
+    {
+        title: "Document Scanner",
+        subtitle: "Scan with Ease",
+        description: "Scan any document instantly with your mobile device by just a few steps. Save as PDF,JPG,ZIP,TXT and Word format.",
+        image: "assets/images/documentScanner.png",
+    },
+    {
+        title: "Sign & Stamp",
+        subtitle: "One-Tap Focus",
+        description: "Draw, scan or import your signature and stamp with a simple touch. Sign and stamp any document with just a single tap!",
+        image: "assets/images/signatureAndStamp.png",
+    },
+    {
+        title: "Batch Scanning",
+        subtitle: "Multiple Page Scan",
+        description: "Scan multiple pages or documents in multiple-scanning mode. Batch all scans as a single document.",
+        image: "assets/images/batchScan.png",
+    },
+    {
+        title: "Advanced Filters",
+        subtitle: "Unique Filters",
+        description: "Apply advanced filters and enhance quality with various custom made filters. Manually edit brightness and contrast by your own choice on the custom filters.",
+        image: "assets/images/advancedFilters.png",
+    },
+    {
+        title: "Export & Share",
+        subtitle: "All-Round Conversion",
+        description: "Export your scans as PDF,JPG,ZIP,TXT and Word.",
+        image: "assets/images/exportAndShare.png",
+    }
+];
