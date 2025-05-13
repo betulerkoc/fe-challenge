8d460f1b4bddbb0c40b1f89320958d226267cfa6
Betul Erkoc <bet.er@hotmail.com>
2025-05-12 21:32:47 -0400

    add svg icons

---
+++ src/components/Icons/index.ts
@@ -0,0 +1,5 @@
+export { default as AdvancedFilter } from './AdvancedFilter';
+export { default as BatchScan } from './BatchScan';
+export { default as DocumentScanner } from './DocumentScanner';
+export { default as Export } from './Export';
+export { default as SignAndStamp } from './SignAndStamp';
