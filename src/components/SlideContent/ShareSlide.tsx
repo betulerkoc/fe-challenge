b10f27e2da9e7126ed92a3ea4293087c77bfcfa8
Betul Erkoc <bet.er@hotmail.com>
2025-05-14 10:28:24 -0400

    add arrow image

--- src/components/SlideContent/ShareSlide.tsx
+++ src/components/SlideContent/ShareSlide.tsx
@@ -3,6 +3,7 @@ import { MenuItem } from '../../types';
 import shareJpgImg from '../../assets/shareJpg.png';
 import sharePdfImg from '../../assets/sharePdf.png';
 import shareTxtImg from '../../assets/shareTxt.png';
+import shareArrowImg from '../../assets/shareArrow.png';
 
 interface ShareSlideProps {
   slide: MenuItem;
@@ -18,21 +19,26 @@ export const ShareSlide: React.FC<ShareSlideProps> = ({ slide }) => (
       />
     </div>
 
-    <div className="absolute inset-x-0 bottom-5 flex justify-center items-center gap-4">
+    <div className="absolute inset-x-0 bottom-5 flex justify-center items-center">
+      <img
+        src={shareArrowImg}
+        alt={'Share'}
+        className="w-[23px] h-[15px] object-contain opacity-0 -mr-2 mt-12 animate-[slideUp_0.8s_ease-out_1000ms_forwards]"
+      />
       <img
         src={sharePdfImg}
         alt={'Share PDF'}
-        className="w-[72px] h-[86px] object-contain opacity-0 animate-[slideUp_0.8s_ease-out_1000ms_forwards]"
+        className="w-[110px] h-[132px] object-contain opacity-0 mx-1 animate-[slideUp_0.8s_ease-out_1250ms_forwards]"
       />
       <img
         src={shareJpgImg}
         alt={'Share JPG'}
-        className="w-[64px] h-[86px] object-contain opacity-0 mb-8 animate-[slideUp_0.8s_ease-out_2000ms_forwards]"
+        className="w-[96px] h-[132px] object-contain opacity-0 mb-12 animate-[slideUp_0.8s_ease-out_1500ms_forwards]"
       />
       <img
         src={shareTxtImg}
         alt={'Share TXT'}
-        className="w-[72px] h-[86px] object-contain opacity-0 animate-[slideUp_0.8s_ease-out_3000ms_forwards]"
+        className="w-[110px] h-[132px] object-contain opacity-0 mx-3 animate-[slideUp_0.8s_ease-out_1750ms_forwards]"
       />
     </div>
   </div>
