c959dbde8f2ab741da038a3c603d3f858d071622
Betul Erkoc <bet.er@hotmail.com>
2025-05-15 07:54:53 -0400

    update font sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
-
-module.exports = {
-    theme: {
-      extend: {
-        fontFamily: {
-          inter: ['Inter', 'sans-serif'],
-        },
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'system-ui', '-apple-system', 'sans-serif'],
       },
+      translate: {
+        '15': '3.75rem',
+        '22': '5.5rem',
+        '28': '7rem',
+      }
     },
-  };
+  },
+  plugins: [],
+}
   
